40367daa0b877eaa5aa8b8aa5fa88523b087fc4a
ROllerozxa <rollerozxa@voxelmanip.se>
2025-03-14 21:22:38 +0100

    Fix

--- windows_release.sh
+++ windows_release.sh
@@ -7,6 +7,10 @@ mkdir -p release
 
 cp principia.exe release/
 
+if [[ -z "${MSYSTEM-}" ]]; then
+	$MINGW_PREFIX/bin/gdk-pixbuf-query-loaders.exe --update-cache
+fi
+
 cd release
 # collect up GTK3 junk to make it work
 PIXBUF_DIR="lib/gdk-pixbuf-2.0/2.10.0"
@@ -18,16 +22,19 @@ SCHEMAS_DIR="share/glib-2.0/schemas"
 mkdir -p $SCHEMAS_DIR
 cp $MINGW_PREFIX/$SCHEMAS_DIR/{gschema.dtd,gschemas.compiled} $SCHEMAS_DIR/
 
-cd ..
+if [[ -z "${MSYSTEM-}" ]]; then
+	list=$($MINGW_PREFIX/bin/ntldd.exe --search-dir $MINGW_PREFIX/bin -R principia.exe | awk '$3 ~ /^Z:\\/ {gsub(/^Z:/, "", $3); gsub(/\\/, "/", $3); print $3}')
+else
+	list=$(ldd principia.exe | grep $MINGW_PREFIX | sed 's/.* => //' | sed 's/ \(.*\)//')
+fi
 
-list=$($MINGW_PREFIX/bin/ntldd.exe --search-dir $MINGW_PREFIX/bin -R principia.exe | awk '$3 ~ /^Z:\\/ {gsub(/^Z:/, "", $3); gsub(/\\/, "/", $3); print $3}')
-for dll in $list;
-do
-	echo $dll
-	cp $dll $2/
+for dll in $list; do
+	cp -v $dll .
 done
+
+cd ..
 
 cp ../packaging/principia_install.nsi .
 cp -r ../packaging/installer/ .
 
-$MINGW_PREFIX/makensis.exe principia_install
+$MINGW_PREFIX/bin/makensis.exe principia_install
