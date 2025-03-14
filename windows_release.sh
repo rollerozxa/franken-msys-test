f5fe3420c5cfcf893e1c0d28816f51a35be9955c
ROllerozxa <rollerozxa@voxelmanip.se>
2025-03-14 21:16:51 +0100

    a

---
+++ windows_release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -eu
+
+# This script assumes you already have built the executable
+
+rm -rf release
+mkdir -p release
+
+cp principia.exe release/
+
+cd release
+# collect up GTK3 junk to make it work
+PIXBUF_DIR="lib/gdk-pixbuf-2.0/2.10.0"
+mkdir -p $PIXBUF_DIR/loaders/
+cp $MINGW_PREFIX/$PIXBUF_DIR/loaders.cache $PIXBUF_DIR/
+cp $MINGW_PREFIX/$PIXBUF_DIR/loaders/libpixbufloader-{jpeg,png}.dll $PIXBUF_DIR/loaders/
+
+SCHEMAS_DIR="share/glib-2.0/schemas"
+mkdir -p $SCHEMAS_DIR
+cp $MINGW_PREFIX/$SCHEMAS_DIR/{gschema.dtd,gschemas.compiled} $SCHEMAS_DIR/
+
+cd ..
+
+list=$($MINGW_PREFIX/bin/ntldd.exe --search-dir $MINGW_PREFIX/bin -R principia.exe | awk '$3 ~ /^Z:\\/ {gsub(/^Z:/, "", $3); gsub(/\\/, "/", $3); print $3}')
+for dll in $list;
+do
+	echo $dll
+	cp $dll $2/
+done
+
+cp ../packaging/principia_install.nsi .
+cp -r ../packaging/installer/ .
+
+$MINGW_PREFIX/makensis.exe principia_install
